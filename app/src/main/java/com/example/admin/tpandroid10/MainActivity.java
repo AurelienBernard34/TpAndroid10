c04d0b46b8dc7f4883bc4d1c8484612f2a374ef6
AurelienBernard34 <aurelienbernard34@gmail.com>
2017-06-14 15:28:03 +0200

    Ajout de + - Enter, Swap, Pop et les fonctionnalités demandés

--- app/src/main/java/com/example/admin/tpandroid10/MainActivity.java
+++ app/src/main/java/com/example/admin/tpandroid10/MainActivity.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Déclaration des boutons
@@ -33,10 +37,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView text_Pile2;
     TextView text_Pile1;
 
+    //Creation stack
+    Stack<Integer> Pile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Pile = new Stack<>();
 
         //Récupération des boutons
         bouton1 = ((Button) findViewById(R.id.touche1));
@@ -56,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bouton_pop = ((Button) findViewById(R.id.touche_pop));
         bouton_clear = ((Button) findViewById(R.id.touche_clear));
 
-        //Ajout des Listener sur les boutons 1 à 9
+        //Ajout des Listener sur les boutons 1 à 9 + enter + fonctionnalités
         bouton1.setOnClickListener(this);
         bouton2.setOnClickListener(this);
         bouton3.setOnClickListener(this);
@@ -66,6 +75,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bouton7.setOnClickListener(this);
         bouton8.setOnClickListener(this);
         bouton9.setOnClickListener(this);
+        bouton_enter.setOnClickListener(this);
+        bouton_clear.setOnClickListener(this);
+        bouton_pop.setOnClickListener(this);
+        bouton_swap.setOnClickListener(this);
+        bouton_plus.setOnClickListener(this);
+        bouton_moins.setOnClickListener(this);
+        //bouton_diviser.setOnClickListener(this); //A revoir
 
         //Récupération des TextView
         text_Courant = ((TextView) findViewById(R.id.text_courant));
@@ -107,6 +123,135 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.touche9:
                 text_Courant.setText(text_Courant.getText()+"9");
                 break;
+            case R.id.touche_enter:
+                entrer();
+                    break;
+            case R.id.touche_clear:
+                clearEcran();
+                Pile.clear();
+                break;
+            case R.id.touche_pop:
+                pop();
+                break;
+            case R.id.touche_swap:
+                swap();
+                break;
+            case R.id.touche_plus:
+                ajouter();
+                break;
+            case R.id.touche_moins:
+                soustraire();
+                break;
+            case R.id.touche_slash:
+                diviser();
+                break;
+
         }
     }
+
+    public void ActualiserPile(Stack<Integer> pile){
+        if (pile.size()>=1) {
+            text_Pile1.setText(pile.get((pile.size() - 1)).toString());
+        }
+        if (pile.size()>=2){
+            text_Pile2.setText(pile.get((pile.size()-2)).toString());
+        }
+        if (pile.size()>=3){
+            text_Pile3.setText(pile.get((pile.size()-3)).toString());
+        }
+        if (pile.size()>=4){
+            text_Pile4.setText(pile.get((pile.size()-4)).toString());
+        }
+    }
+
+    public void clearEcran() {
+        text_Courant.setText("");
+        text_Pile1.setText("");
+        text_Pile2.setText("");
+        text_Pile3.setText("");
+        text_Pile4.setText("");
+    }
+
+    public void pop(){
+        if(!Pile.empty()) {
+            Pile.pop();
+            clearEcran();
+            ActualiserPile(Pile);
+        }
+    }
+
+    public void swap(){
+        int number1, number2;
+        number1 = Pile.peek();
+        Pile.pop();
+        number2 = Pile.peek();
+        Pile.pop();
+
+        Pile.push(number1);
+        Pile.push(number2);
+
+        clearEcran();
+        ActualiserPile(Pile);
+    }
+
+    public void ajouter(){
+            entrer();
+        if (Pile.size()>=2) {
+            int num1, num2, total;
+            num1 = Pile.peek();
+            Pile.pop();
+            num2 = Pile.peek();
+            Pile.pop();
+            total = num1 + num2;
+            Pile.push(total);
+            clearEcran();
+            ActualiserPile(Pile);
+        }
+    }
+
+    public void soustraire() {
+            entrer();
+        if (Pile.size() >= 2) {
+            int num1, num2, total;
+            num1 = Pile.peek();
+            Pile.pop();
+            num2 = Pile.peek();
+            Pile.pop();
+            total = num1 - num2;
+            Pile.push(total);
+            clearEcran();
+            ActualiserPile(Pile);
+
+        }
+    }
+
+    public void diviser(){
+        entrer();
+        if (Pile.size() >= 2) {
+            int num1, num2, total;
+            num1 = Pile.peek();
+            Pile.pop();
+            num2 = Pile.peek();
+            Pile.pop();
+            total = num1 / num2;
+            Pile.push(total);
+            clearEcran();
+            ActualiserPile(Pile);
+
+        }
+    }
+
+
+
+    public void entrer() {
+        if (!text_Courant.getText().toString().equals("")) {
+            int val = Integer.valueOf((text_Courant.getText()).toString());
+            Pile.push(val);
+            text_Courant.setText("");
+            ActualiserPile(Pile);
+        }
+    }
+
+
+
 }
